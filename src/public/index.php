23d663340ba1e52bc474021528974c3cd8f12552
Schmidt Zoltán <zoltanschmidt@protonmail.com>
2022-08-06 00:12:39 +0200

    static html serve skeleton

--- src/public/index.php
+++ src/public/index.php
@@ -1,3 +1,53 @@
 <?php
 
-echo "hello world";
+use Phalcon\Di\FactoryDefault;
+use Phalcon\Loader;
+use Phalcon\Mvc\Application;
+use Phalcon\Mvc\View;
+use Phalcon\Url;
+
+// path constants
+define('BASE_PATH', dirname(__DIR__));
+define('APP_PATH', BASE_PATH . '/app');
+
+$directories = [
+    APP_PATH . '/controllers/',
+    APP_PATH . '/models/',
+];
+
+// loader
+$loader = new Loader();
+$loader->registerDirs($directories);
+$loader->register();
+
+// dependency injector
+$di = new FactoryDefault();
+$di->set(
+    'view',
+    function () {
+        $view = new View();
+        $view->setViewsDir(APP_PATH . '/views/');
+
+        return $view;
+    }
+);
+$di->set(
+    'url',
+    function () {
+        $url = new Url();
+        $url->setBaseUri('/');
+
+        return $url;
+    }
+);
+
+// application handler
+$application = new Application($di);
+
+// request handling
+try {
+    $response = $application->handle($_SERVER["REQUEST_URI"]);
+    $response->send();
+} catch (\Exception $e) {
+    echo 'Exception: ', $e->getMessage();
+}
